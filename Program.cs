2e896b91bbe1b0c031b668a695145f293ac34a24
JonathanFloresPacheco <33069854+JonathanFloresPacheco@users.noreply.github.com>
2018-05-22 21:53:14 -0500

    Generar Json de sopomex

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadJson01
+{
+    class Program
+    {
+        public static PropiedasdesJson rdJson = new PropiedasdesJson();
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                //List<listpropiedades> sepoList = new List<listpropiedades>();
+                //Leemos el directorio en busca de archivos
+                DirectoryInfo d = new DirectoryInfo("C:\\tuvotocuenta\\Archivos\\Json");
+                //Instancia de XML
+                //XmlDocument xDoc = new XmlDocument();
+                //XmlNode VlDirXml, VlDirXml2;
+                foreach (FileInfo file in d.GetFiles())
+                {
+                    String archivo = Convert.ToString(file.Name);
+                    //lbArchivo.Text += archivo + " | ";
+                    Console.WriteLine("Seleccione el menu");
+                    Console.WriteLine("1.- Entidad Federativa        2.-Municipio        3.-Salir");
+                    Console.WriteLine("Ingrese el numero a elegir:");
+                    string path = @"C:\tuvotocuenta\Archivos\Json\" + archivo;
+                    int Res =Convert.ToInt32(Console.ReadLine());
+                    switch(Res)
+                    {
+                        case 1:
+                            string RetVal0= rdJson.BusquedaporEntidad(path, "Aguascalientes");
+                            Console.WriteLine("Valores: ");
+                            Console.WriteLine(RetVal0);
+                            Console.WriteLine("******************************************************");
+                            Console.ReadLine();
+                            break;
+                        case 2:
+                            string RetVal1 = rdJson.BusquedaporMunicipio(path, "Nahuatzen");
+                            Console.WriteLine(RetVal1);
+                            Console.WriteLine("******************************************************");
+                            Console.ReadLine();
+                            break;
+                        case 3:
+                            break;
+                    }
+                }
+            }
+            catch (Exception erc)
+            {
+                Console.WriteLine("Error "+erc.Message);
+            }
+        }
+    }
+}
---
+++ PropiedasdesJson.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadJson01
+{
+    class PropiedasdesJson
+    {
+        public string D_mnpio { get; set; }
+        public string D_estado { get; set; }
+        public string D_zona { get; set; }
+
+
+        public string BusquedaporEntidad(string path,string estado)
+        {
+            string tempValue = "";
+            int Validacion = 0;
+            JArray jsonResponse = JArray.Parse(File.ReadAllText(path));
+            foreach (JObject content in jsonResponse.Children<JObject>())
+            {
+                Validacion = 0;
+                foreach (JProperty prop in content.Properties())
+                {
+
+                    //if(prop.Name.Equals("d_estado") && prop.Value.Equals(estado))
+                    //{
+                    //    tempValue += prop.Value.ToString();                         
+                    //}
+
+                    if (prop.Name == "d_estado" && prop.Value.ToString() == estado || Validacion == 1)
+                    {
+                        if (Validacion == 0)
+                        {
+                            tempValue += prop.Value.ToString() + "|";
+                            Validacion = 1;
+                        }
+                        else if (Validacion == 1)
+                        {
+                            tempValue += prop.Value.ToString() + "|";
+                            Validacion = 2;
+                        }
+                    }
+                }
+            }
+            return tempValue;
+        }
+        public string BusquedaporMunicipio(string path,string municipio)
+        {
+            string tempValue = "";
+            int Validacion = 0;
+            JArray jsonResponse = JArray.Parse(File.ReadAllText(path));
+            foreach (JObject content in jsonResponse.Children<JObject>())
+            {
+                Validacion = 0;
+                foreach (JProperty prop in content.Properties())
+                {
+                    if (prop.Name=="D_mnpio" && prop.Value.ToString()== municipio || Validacion == 1)
+                    {
+                        if(Validacion==0)
+                        {
+                            tempValue += prop.Value.ToString() + "|";
+                            Validacion = 1;
+                        }
+                        else if(Validacion==1)
+                        {
+                            tempValue += prop.Value.ToString() + "|";
+                            Validacion = 0;
+                        }
+                    }
+                }
+            }
+            return tempValue;
+        }
+    }
+}
